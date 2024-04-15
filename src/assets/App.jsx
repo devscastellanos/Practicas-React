3260a1c404378b7ffec0b7b71e5b12a015e83bc7
devscastellanos <scastellanos1220@gmail.com>
2024-04-15 15:21:52 -0500

    Add first class

---
+++ src/assets/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'MadridTotal_',
+    name: 'Cristiano Ronaldo',
+    isFollowing: true
+  },
+  {
+    userName: 'Batman_GothamBW',
+    name: 'Batman 🦇',
+    isFollowing: false
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
